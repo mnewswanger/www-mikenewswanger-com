4359caba9e14726fb6864cca4aef27fef4d96666
Mike Newswanger <mike@mikenewswanger.com>
2018-07-20 17:40:20 -0400

    Remove build script

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
-hugo --source ${DIR} --cleanDestinationDir \
- && time rsync -rv --delete ${DIR}/public/ root@www.mikenewswanger.com:/var/www/www.mikenewswanger.com/html
-
